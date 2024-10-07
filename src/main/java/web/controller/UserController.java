fd3b2fc513209d9a242f54bcfd54f261d2e31d1d
KonstKseniya <kolody.xenia@yandex.ru>
2024-10-07 07:40:04 +0300

    Update UserController.java

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -19,8 +19,12 @@ import java.util.List;
 @RequestMapping("/")
 public class UserController {
 
+    private final UserService userService;
+
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public String showUsersList(Model model) {
